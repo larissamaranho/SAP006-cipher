def2eb2d27c0c0be74dcd6a07f51cc5e998ac3aa
larissamaranho <larissamaranhor@gmail.com>
2021-06-09 12:23:46 -0300

    First Commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,22 @@
-const cipher = {
-  // ...
+const cipher = { 
+  encode: function (offset, string){
+    const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","Z"]
+    const inputOriginal = document.getElementById('inputCaixaTexto')
+    const cifrador = document.getElementById('caixaDeTexto')
+    let resultado = document.getElementById('resultado')
+    const chave = document.getElementById('chaveOffset')
+
+    alfabeto.map(letra => {
+      console.log(`A letra: ${letra} tem o código: ${letra.charCodeAt(0)}`)
+    })
+
+    cifrador.addEventListener("submit", function(e){
+      e.preventDefault()
+      //console.log(inputOriginal.value)
+      resultado.innerHTML = inputOriginal.value
+      resultado = [...inputOriginal.value]
+    })
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
+window.addEventListener("load", function () {
 
-console.log(cipher);
+    cipher.encode();
+})
+
+let caixaTexto = document.getElementById("inputCaixaTexto");
+caixaTexto.addEventListener("keypress", event => {
+let caixaTextoValor = caixaTexto.value
+console.log(caixaTextoValor);
+})
+
+let offset = document.getElementById("chaveOffset");
+offset.addEventListener("click", event => {
+let offsetChave = offset.value
+console.log(offsetChave);
+})
+
+let caixaResultado = document.getElementById("code");
+caixaResultado.addEventListener("keypress" , event => {
+let caixaTexotoResultado = caixaResultado.value
+console.log(caixaTexotoResultado);
+})
+
+let buttonEncrypty = document.getElementById("encrypt");
+buttonEncrypty.addEventListener("click", event => {
+let codificarBotao = buttonEncrypty.value
+console.log(codificarBotao);
+})
+
+let buttonDecrypt = document.getElementById("decrypt");
+buttonDecrypt.addEventListener("click", event => {
+let descodificarBotao = buttonDecrypt.value
+console.log(descodificarBotao);
+})

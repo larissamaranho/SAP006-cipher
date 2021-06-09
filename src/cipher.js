fd3a919868cf2d062e592950a0d41163b3ea2caa
larissamaranho <larissamaranhor@gmail.com>
2021-06-09 14:29:17 -0300

    First Commit

--- src/cipher.js
+++ src/cipher.js
@@ -19,4 +19,15 @@ const cipher = {
   }
 };
 
+
+/*
+
+A  B  C  D  E  F  G  H  I  J  K  L  M
+0  1  2  3  4  5  6  7  8  9  10 11 12
+
+N  O  P  Q  R  S  T  U  V  X  W  X  Y  Z 
+13 14 15 16 17 18 19 20 21 22 23 24 25 26 
+
+*/
+
 export default cipher;
